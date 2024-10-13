75eaee26ff624ee43ac810146d9bd1043a51cc04
Jonathan Ryan <jonathan071106@gmail.com>
2024-10-13 08:23:43 +0700

    merge sort setup

---
+++ src/merge_sort.py
@@ -0,0 +1,10 @@
+from utils import create_unordered_list
+
+
+def merge_sort(unsorted_list: list[int]) -> list[int]:
+    pass
+
+
+if __name__ == '__main__':
+    unsorted = create_unordered_list(1000)
+    merge_sort(unsorted)
